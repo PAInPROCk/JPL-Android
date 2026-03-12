f09878c4b61b41a95d834e95ae1b212d45405f10
rahulilangovan <rahulilangovan1749@gmail.com>
2026-03-13 01:56:50 +0530

    Initial commit - Android app

---
+++ app/src/main/java/com/example/jpl2/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.jpl2;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = findViewById(R.id.webview);
+
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        webView.setWebViewClient(new WebViewClient());
+
+        webView.loadUrl("http://192.168.29.135:3000");
+    }
+}
