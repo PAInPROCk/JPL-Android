bad633389d74c8e67b530f9ab7f208273f0dc3f8
PAInPROCk <2601prathmeshbhadwalkar@gmail.com>
2026-04-16 23:33:16 +0530

    Feature: Added player info page

--- app/src/main/java/com/example/jpl2/activities/PlayerDetailsActivity.java
+++ app/src/main/java/com/example/jpl2/activities/PlayerDetailsActivity.java
@@ -3,6 +3,7 @@ package com.example.jpl2.activities;
 import static com.example.jpl2.api.ApiClient.BASE_URL;
 
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -14,25 +15,47 @@ import com.example.jpl2.R;
 public class PlayerDetailsActivity extends AppCompatActivity {
 
     ImageView playerImage;
-    TextView playerName, playerRole, playerPrice;
+    TextView playerName, playerRole, playerPrice, playerNickName, playerStyle, playerAge, playerGender, playerTeam, playerCategory;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_player_details);
 
+        //Image
         playerImage = findViewById(R.id.playerImage);
+
+        //text views
         playerName = findViewById(R.id.playerName);
+        playerNickName = findViewById(R.id.playerNickName);
+        playerCategory = findViewById(R.id.playerCategory);
+        playerStyle = findViewById(R.id.playerStyle);
         playerRole = findViewById(R.id.playerRole);
-        playerPrice = findViewById(R.id.playerPrice);
+        playerAge = findViewById(R.id.playerAge);
+        playerPrice = findViewById(R.id.playerBasePrice);
+        playerGender = findViewById(R.id.playerGender);
+        playerTeam = findViewById(R.id.playerTeams);
 
         // GET DATA FROM INTENT
         String name = getIntent().getStringExtra("name");
-        String image = getIntent().getStringExtra("image");
-        int price = getIntent().getIntExtra("price", 0);
+        String nickName = getIntent().getStringExtra("nickname");
+        String category = getIntent().getStringExtra("category");
+        String style = getIntent().getStringExtra("style");
         String role = getIntent().getStringExtra("role");
+        int age = getIntent().getIntExtra("age", 0);
+        int price = getIntent().getIntExtra("price", 0);
+        String gender = getIntent().getStringExtra("gender");
+        String teams = getIntent().getStringExtra("teams");
+        String image = getIntent().getStringExtra("image");
 
         playerName.setText(name);
+        playerNickName.setText(nickName);
+        playerCategory.setText(category);
+        playerStyle.setText(style);
+        playerRole.setText(role);
+        playerAge.setText(String.valueOf(age));
+        playerGender.setText(gender);
+        playerTeam.setText(TextUtils.isEmpty(teams) ? "No teams recorded" : teams);
         playerRole.setText("Role: " + role);
         playerPrice.setText("₹ " + price);
 
